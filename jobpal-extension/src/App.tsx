d7be0c049739c84a2490accd582928f10b6c9250
Gordon Sau <t.sau@student.unsw.edu.au>
2022-07-17 18:04:24 +1000

    use a href instead of Link to open edit.html

--- jobpal-extension/src/App.tsx
+++ jobpal-extension/src/App.tsx
@@ -1,14 +1,11 @@
 import './App.css';
-import {
-  Link,
-} from 'react-router-dom';
 
 import Home from './Pages/HomePage';
 
 function App() {
   return (<>
     <Home/>
-    <Link to="edit.html" target="_blank" rel="noopener noreferrer">Edit Profile</Link>
+    <button><a href="/edit.html" target="_blank" rel="noopener noreferrer">Edit Profile</a></button>
   </>);
 }
 
