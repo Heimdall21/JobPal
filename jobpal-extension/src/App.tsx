b47b71153fc4192692807a7a7bbbe8da2fc141ec
Gordon Sau <t.sau@student.unsw.edu.au>
2022-07-17 16:18:17 +1000

    create link to edit page

--- jobpal-extension/src/App.tsx
+++ jobpal-extension/src/App.tsx
@@ -1,30 +1,15 @@
 import './App.css';
 import {
-  MemoryRouter as Router,
-  Routes,
-  Route,
-  Navigate,
   Link,
-  useNavigate
 } from 'react-router-dom';
 
 import Home from './Pages/HomePage';
-import Edit from './Pages/EditPage';
-import { useState } from 'react';
 
 function App() {
-  const [route, setRoute] = useState('/');
-
-  return (
-  <Router>
-      <Navigate to={route} />
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/edit" element={<Edit/>}/>
-        </Routes>
-        {(route !== '/edit')? <button onClick={()=>setRoute("/edit")}>Edit Profile</button>: <></>}
-    </Router>
-  );
+  return (<>
+    <Home/>
+    <Link to="edit.html" target="_blank" rel="noopener noreferrer">Edit Profile</Link>
+  </>);
 }
 
 export default App;
