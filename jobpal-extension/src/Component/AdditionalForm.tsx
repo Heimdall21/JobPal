d5b413985637ba264e305d4347c74a5146c2551c
Gordon Sau <t.sau@student.unsw.edu.au>
2022-08-08 17:38:46 +1000

    implementing CommonForm

--- jobpal-extension/src/Component/AdditionalForm.tsx
+++ jobpal-extension/src/Component/AdditionalForm.tsx
@@ -1,3 +1,11 @@
-export default function AdditionalForm({addFields, removeFields, updateFields, data}) {
+import { AdditionalPrefillData } from "../Lib/StorageType";
+
+export default function AdditionalForm({addFields, removeFields, updateFields, data}: {
+    addFields: (key: string, val: string)=>boolean,
+    removeFields: (key: string)=>void,
+    data: AdditionalPrefillData,
+    updateFields: (key: string, val: string)=>void
+}) {
+    console.log(data);
     return <></>;
 }
--- jobpal-extension/src/Component/GeneralForm.tsx
+++ jobpal-extension/src/Component/GeneralForm.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import {CommonPrefillData, ExtendedCommonPrefillData} from '../Lib/StorageType';
 import AdditionalForm from './AdditionalForm';
+import { DateString } from './DateString';
 import styles from "./Form.module.css";
 
 export default function GeneralForm({commonData, setCommonData}: {
@@ -15,6 +16,14 @@ export default function GeneralForm({commonData, setCommonData}: {
             [field]: value,
             additional: additionalData
         })
+    }
+
+    function removeGeneralField<K extends keyof CommonPrefillData>(field: K) {
+        const {[field]: _, ...rest} = generalData;
+        setCommonData({
+            ...rest,
+            additional: additionalData
+        });
     }
 
     function addAdditionalField(key: string, value: string):boolean {
@@ -49,53 +58,110 @@ export default function GeneralForm({commonData, setCommonData}: {
         })
     }
 
-    function toFormInputField<K extends keyof Omit<CommonPrefillData,'dateOfBirth'| 'yearOfGrad' |'sex'>>(field: K, labelText: string) {
+    function FormInputField<K extends keyof Omit<CommonPrefillData,'dateOfBirth'| 'yearOfGrad' |'sex'>>(field: K, labelText: string) {
+        const handleChange = (e: ChangeEvent<HTMLInputElement>)=>{
+            const val = e.target.value;
+            console.log(field, val);
+            if (val === '') {
+                removeGeneralField(field);
+                return;
+            }
+            updateGeneralField(field, val);
+        }
+
         return (<>
             <label htmlFor={field}>{labelText}</label>
-            <input type={"text"} id={field} name={field} value={generalData[field] || ''}></input>
+            <input type={"text"} id={field} name={field} value={generalData[field] || ''} onChange={handleChange}></input>
         </>);
     }
 
-    return (
-    <div className={styles.FormContainer}>
-        <div className='Category'>Personal Information</div>
-        {toFormInputField("givenName", "First Name")}
-        {toFormInputField("familyName", "Last Name")}
-        {toFormInputField("email", "Email")}
+    function FormSexField() {
+        const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+            const val = e.target.value;
+            console.log(val);
+            if (val === 'M' || val === 'F' || val === 'X') {
+                updateGeneralField('sex', val);
+            } else {
+                removeGeneralField('sex');
+            }
+        }
 
+        return (<>
         <label htmlFor='sex'>Sex</label>
-        <select id="sex" name="sex" value={generalData.sex || ""}>
+        <select id="sex" name="sex" value={generalData.sex || ""} onChange={handleChange}>
             <option value="" disabled={true} hidden={true}>Please Choose Your Sex</option>
             <option value="M">Male</option>
             <option value="F">Female</option>
             <option value="X">Other</option>
         </select>
+        </>);
+    }
 
+    function FormDateOfBirthField() {
+        const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+            const val = e.target.value;
+            console.log(val);
+            if (DateString.validate(val)) {
+                updateGeneralField('dateOfBirth', val);
+            } else if (val.trimStart() === '') {
+                removeGeneralField('dateOfBirth');
+            } else {
+                console.error("not a date");
+            }
+        }
+
+        return (<>
         <label htmlFor={'dateOfBirth'}>Date Of Birth</label>
         <input type={"date"} id={'dateOfBirth'} name={'dateOfBirth'} value={
-            generalData.dateOfBirth === undefined ? '' : dateToString(generalData.dateOfBirth)
-        }></input>
+            generalData.dateOfBirth === undefined ? '' : (DateString.validate(generalData.dateOfBirth)??'')
+        }
+        onChange={handleChange}></input>
+        </>);
+    }
 
-        {toFormInputField("address", "Address")}
-        {toFormInputField("postalCode", "Postal Code")}
+    function FormYearOfGradField() {
+        const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+            const val = e.target.value;
+            const num = parseInt(val, 10);
+            if (!isNaN(num) && num > 0 && num <= 9999) {
+                updateGeneralField('yearOfGrad', num);
+            } else {
+                removeGeneralField('yearOfGrad');
+            }
+        }
+
+        return (<>
+        <label htmlFor={'yearOfGrad'}> Year of Graduation</label>
+        <input type="number" min="1900" max="2099" step="1" value={generalData.yearOfGrad === undefined? '': generalData.yearOfGrad.toString()} placeholder={"Predicted Year of Graduation"} onChange={handleChange}></input>
+        </>)
+    }
+
+    return (
+    <div className={styles.FormContainer}>
+        <div className='Category'>Personal Information</div>
+        {FormInputField("givenName", "First Name")}
+        {FormInputField("familyName", "Last Name")}
+        {FormInputField("email", "Email")}
+
+        {FormSexField()}
+
+        {FormDateOfBirthField()}
+
+        {FormInputField("address", "Address")}
+        {FormInputField("postalCode", "Postal Code")}
 
         <div className='Category'>Education</div>
-        {toFormInputField("university", "University")}
-        {toFormInputField("degree", "Degree")}
+        {FormInputField("university", "University")}
+        {FormInputField("degree", "Degree")}
+        {FormYearOfGradField()}
 
-        <label htmlFor={'yearOfGrad'}> Year of Graduation</label>
-        <input type="number" min="1900" max="2099" step="1" value={generalData.yearOfGrad === undefined? '': generalData.yearOfGrad.toString()} placeholder={"Predicted Year of Graduation"}></input>
 
         <div className='Category'>Links</div>
-        {toFormInputField("github", "Github Link")}
-        {toFormInputField("linkedin", "LinkedIn Link")}
+        {FormInputField("github", "Github Link")}
+        {FormInputField("linkedin", "LinkedIn Link")}
 
         <div className='Category'>Additional Information</div>
         <AdditionalForm data={additionalData} addFields={addAdditionalField} removeFields={removeAdditionalField} updateFields={updateAdditionalField}/>
     </div>
     );
-}
-
-function dateToString(date: Date):string {
-    return date.toISOString().slice(0, 10);
 }
