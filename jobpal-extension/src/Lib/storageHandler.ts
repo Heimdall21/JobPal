d3318fa97a8286a886d7929270057dac9436bdf3
Gordon Sau <t.sau@student.unsw.edu.au>
2022-08-09 13:59:31 +1000

    add helper functions for listening changes in the data

--- jobpal-extension/src/Lib/storageHandler.ts
+++ jobpal-extension/src/Lib/storageHandler.ts
@@ -11,3 +11,20 @@ export const getPrefillData = (getCallback: (data: PrefillData)=>void) => {
 export const storePrefillData = (data: PrefillData, setCallback=()=>{}) => {
     return chrome.storage.sync.set({[profileQueryKey]: data}, setCallback);
 }
+
+export const onUpdatePrefillData = (callback: ((data:PrefillData)=>void))=>{
+    return ()=>{
+        const onUpdate = (
+            changes:{ [key: string]: chrome.storage.StorageChange }, 
+            areaName: string
+        ) => {
+            if (areaName === 'sync' && profileQueryKey in changes) {
+                const {newValue} = changes[profileQueryKey];
+                callback(newValue!);
+            }
+        };
+        chrome.storage.onChanged.addListener(onUpdate);
+        return ()=>chrome.storage.onChanged.removeListener(onUpdate);
+    }
+    // usecase: useEffect(onUpdatePrefillData, []);
+}
