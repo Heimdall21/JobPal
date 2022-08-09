5e21ff20d364ccf9b30220e2608d14a6aad6ffe4
Gordon Sau <t.sau@student.unsw.edu.au>
2022-08-09 14:00:47 +1000

    set nextId and add removeRow

--- jobpal-extension/src/Component/SpecificForm.tsx
+++ jobpal-extension/src/Component/SpecificForm.tsx
@@ -1,13 +1,17 @@
-import { ViewAdditionalType, ViewSpecificData } from "../Lib/ViewFormType"
+import { ViewAdditionalType, ViewSpecificData } from "./ViewFormType"
 import AdditionalForm from "./AdditionalForm";
 
 export default function SpecificForm({data, setData}: {
     data: ViewSpecificData[], 
     setData: React.Dispatch<React.SetStateAction<ViewSpecificData[]>>
 }) {
-    const nextId = Math.max(...data.map(val=>val.id),0);
+    const nextId = Math.max(...data.map(val=>val.id),-1)+1;
     const addRow = ()=>{
             setData([...data, {id: nextId, url:'', companyName:'', role: '', shortcut:'', additional: []}]);
+    }
+
+    const removeRow = (targetId:number)=> {
+        return ()=>setData(data.filter(v=>v.id!==targetId));
     }
 
     const handleDataChange = (targetId: number)=>{
@@ -21,16 +25,17 @@ export default function SpecificForm({data, setData}: {
     }
 
     return (<>
-        <button type="button" onClick={addRow}></button>
-        {data.map(({id, url, additional, ...rest})=><FormRow key={id} _key={url} val={rest} additional={additional} setData={handleDataChange(id)}/>)}
+        <button type="button" onClick={addRow}>Add URL</button>
+        {data.map(({id, url, additional, ...rest})=><FormRow key={id} _key={url} val={rest} additional={additional} setData={handleDataChange(id)} removeRow={removeRow(id)}/>)}
     </>);
 }
 
-function FormRow({_key, val, additional, setData}:{
+function FormRow({_key, val, additional, setData, removeRow}:{
     _key: string,
     val: Omit<ViewSpecificData, 'id' | 'url' | 'additional'>,
     additional: ViewAdditionalType,
-    setData: <K extends keyof ViewSpecificData>(key: K, val: ViewSpecificData[K])=>void
+    setData: <K extends keyof ViewSpecificData>(key: K, val: ViewSpecificData[K])=>void,
+    removeRow: ()=>void
 }) {
 
     const handleValueChange = <K extends keyof Omit<ViewSpecificData, 'id'| 'additional'>>(key: K) => {
@@ -40,6 +45,7 @@ function FormRow({_key, val, additional, setData}:{
     }
 
     return (<>
+        <button type="button" onClick={removeRow}>Remove URL</button>
         <input type="text" value={_key} placeholder={"url"} onChange={handleValueChange('url')}></input>
         <input type="text" value={val.companyName} placeholder={"companyName"} onChange={handleValueChange('companyName')}></input>
         <input type="text" value={val.role} placeholder={"role"} onChange={handleValueChange('role')}></input>
