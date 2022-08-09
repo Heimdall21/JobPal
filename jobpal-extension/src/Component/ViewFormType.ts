3cfcdbf775fc40524bc724248283f9e3f57ee8d8
Gordon Sau <t.sau@student.unsw.edu.au>
2022-08-09 13:59:54 +1000

    move ViewFormType.ts to Component

---
+++ jobpal-extension/src/Component/ViewFormType.ts
@@ -0,0 +1,26 @@
+export interface ViewCommonData {
+    givenName: string,
+    additionalName: string,
+    familyName: string,
+    email: string,
+    sex: 'M'| 'F' | 'X'|'',
+    dateOfBirth: string,
+    address: string,
+    postalCode: string,
+    university: string,
+    degree: string,
+    yearOfGrad: string,
+    github: string,
+    linkedin: string
+}
+
+export type ViewAdditionalType = {id: number, key: string, value: string}[];
+
+export interface ViewSpecificData {
+    id: number,
+    url: string,
+    companyName: string,
+    role: string,
+    shortcut: string,
+    additional: ViewAdditionalType
+}
