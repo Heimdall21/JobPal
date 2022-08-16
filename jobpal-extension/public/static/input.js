080620bd3052c86e9fff7316c87621c16c372740
Joanne <joannechen84@gmail.com>
2022-08-16 21:10:01 +1000

    added find input by label

--- jobpal-extension/public/static/input.js
+++ jobpal-extension/public/static/input.js
@@ -1,22 +1,27 @@
 //debugger;
 console.log("content script running");
 var inputElements = document.getElementsByTagName("input");
-console.log(inputElements);
 
 input_mapping = {
-  "first_name": ".*f.*name",
-  "last_name": ".*l.*name",
-  "preferred_name": ".*pref.*name",
-  "email": ".*email.*",
-  "phone": ".*phone.*",
-  "location": ".*loc.*"
+  "first_name": [".*fname.*", ".*first_name.*", ".*first.*name.*"],
+  "last_name": [".*lname.*", ".*last_name.*", ".*last.*name.*"],
+  "preferred_name": [".*prefname.*"],
+  "email": [".*email.*"],
+  "phone": [".*phone.*"],
+  "location": [".*loc.*"]
 }
 
-for (var ind = 0; ind < inputElements.length; ind++) {
-  console.log(inputElements[ind].name)
-  for (var field in input_mapping) {
-    if (inputElements[ind].name.match(input_mapping[field])) {
-      inputElements[ind].value = field
-    }
+var inputDescriptionElements = document.getElementsByTagName("label");
+
+var inputs = [];
+
+for (var desInd = 0; desInd < inputDescriptionElements.length; desInd++) {
+  var inputElement = document.getElementById(inputDescriptionElements[desInd].htmlFor) || document.getElementsByName(inputDescriptionElements[desInd].htmlFor)[0]
+  if (!inputElement) {
+    inputElement = inputDescriptionElements[desInd].querySelector("select, input:not([type='hidden'])")
+  }
+  if (inputElement) {
+    inputs.push([inputDescriptionElements[desInd].innerText, inputElement])
   }
 }
+console.log(inputs)
