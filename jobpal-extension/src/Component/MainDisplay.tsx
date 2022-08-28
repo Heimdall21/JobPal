46a3227a9085acc92dced79b889015f8532f5546
Jae Jeon <jaejeon@192-168-1-103.tpgi.com.au>
2022-08-28 16:25:01 +1000

    [JOBPAL-19] [Jae] Can add components onto App.tsx

--- jobpal-extension/src/Component/MainDisplay.tsx
+++ jobpal-extension/src/Component/MainDisplay.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <div>
-    <h1>Hello, Welcome to React and TypeScript</h1>
-  </div>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <div>
+//     <h1>Hello, Welcome to React and TypeScript</h1>
+//   </div>,
+//   document.getElementById("root")
+// );
 
 function MainDisplay() {
   return (
