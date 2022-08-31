4b9c6de6f47efff798c3d60035b4d8312622a83f
Jae Jeon <jaejeon@192-168-1-103.tpgi.com.au>
2022-08-31 13:40:40 +1000

    [JOBPAL-19] [Jae] Using dummy data to display Prefill sections list

--- jobpal-extension/src/Component/PrefillSection/PrefillSection.tsx
+++ jobpal-extension/src/Component/PrefillSection/PrefillSection.tsx
@@ -4,32 +4,34 @@ import React from "react";
 // import MainDisplay from './Component/MainDisplay';
 import styles from "./PrefillSection.module.css";
 
+// section_fields: {
+//   title: 'Mr',
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   country: 'Australia',
+//   mobile: '0404040404',
+//   emailAddress: 'john.doe@testemail.com'
+// }
+
 
 interface Props {
-  title: string;
-  border: string;
-  color: string;
-  children?: React.ReactNode;
-  height: string;
-  radius: string;
-  width: string;
+  section_title: string;
+  section_fields?: any,
 }
 
+
 const PrefillSection: React.FC<Props> = ({
-  title, 
-  border, 
-  color, 
-  children,
-  height,
-  radius,
-  width
+  section_title, 
+  section_fields,
 }) => {
 
   return (
     // <div className={`${styles._section_body} ${styles.bottom_margin}`} >
     <div className='_section_body .bottom_margin' >
-      <h1 className='_section_title'>{title}</h1>
-
+      <h1 className='_section_title'>{section_title}</h1>
+      <h2 className="_section_field">{section_fields.title}</h2>
+      <h2 className="_section_field">{section_fields.firstName}</h2>
+      <h2 className="_section_field">{section_fields.lastName}</h2>
     </div>
   );
 }
