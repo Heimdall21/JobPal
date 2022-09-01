5cb4cbfec38e5087015574512f6c64a0a095e09a
Gordon Sau <t.sau@student.unsw.edu.au>
2022-09-01 14:12:53 +1000

    execute listener.js by injecting from the background script

--- jobpal-extension/public/background.js
+++ jobpal-extension/public/background.js
@@ -1,5 +1,10 @@
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
+    target: {tabId: tab.id, allFrames: true},
+    files: ['listener.bundle.js']
+  });
+
+  chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['content.bundle.js']
   });
