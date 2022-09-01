5c93bab44edf4ddf25e8cb4fa94367213bd4183d
Gordon Sau <t.sau@student.unsw.edu.au>
2022-09-01 15:38:18 +1000

    debugging with listener.ts

--- jobpal-extension/src/ContentScripts/listener.ts
+++ jobpal-extension/src/ContentScripts/listener.ts
@@ -1,3 +1,8 @@
 import { getLabelInputPairs } from "./input";
-
+const element = document.createElement('h1');
+element.innerText = "Hello World";
+const body = document.querySelector('body');
+if (body !== null) {
+    body.appendChild(element);
+}
 console.log(getLabelInputPairs(document));
