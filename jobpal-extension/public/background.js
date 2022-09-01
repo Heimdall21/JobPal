11041f538927c4cfb39d99569e52ecdf90e01087
Gordon Sau <t.sau@student.unsw.edu.au>
2022-09-01 19:30:28 +1000

    inject the content script statically so it works for iframes in different origin

--- jobpal-extension/public/background.js
+++ jobpal-extension/public/background.js
@@ -1,10 +1,5 @@
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
-    target: {tabId: tab.id, allFrames: true},
-    files: ['listener.bundle.js']
-  });
-
-  chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['content.bundle.js']
   });
