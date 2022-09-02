cc1bab8bb49b9ca8a13698592245fbdcc5c8940e
Gordon Sau <t.sau@student.unsw.edu.au>
2022-09-02 12:28:59 +1000

    change .js to typescripts files

--- jobpal-extension/webpack.config.js
+++ jobpal-extension/webpack.config.js
@@ -12,9 +12,9 @@ module.exports = {
     },
     entry: {
         index: path.resolve(__dirname, "./src/index.tsx"),
-        content: path.resolve(__dirname, "./public/content.js"),
+        content: path.resolve(__dirname, "./public/content.tsx"),
         input: path.resolve(__dirname, "./src/ContentScripts/input.ts"),
-        background: path.resolve(__dirname, "./public/background.js"),
+        background: path.resolve(__dirname, "./public/background.ts"),
         listener: path.resolve(__dirname, "./src/ContentScripts/listener.ts")
         // options: path.resolve(__dirname, "./src/index-options.js"),
         // foreground: path.resolve(__dirname, "./src/index-foreground.js")
